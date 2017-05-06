38238cffd7a46d1fb7616fb83d88038edf6d57de
Leonardo Carrasco <lcarrasco@nearsoft.com>
2017-05-05 18:16:51 -0600

    create click listener for colored buttons, and compare sequence of buttons pressed with random generated sequence

--- app/src/main/java/com/thesnowgoose/simongame/MainActivity.java
+++ app/src/main/java/com/thesnowgoose/simongame/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.thesnowgoose.simongame.utilities.BtnColors;
 import com.thesnowgoose.simongame.utilities.SynchronyLights;
@@ -16,14 +17,20 @@ import java.util.Random;
 
 import static com.thesnowgoose.simongame.utilities.BtnColors.*;
 
-public class MainActivity extends AppCompatActivity implements SynchronyLights.HighLightButton {
+public class MainActivity extends AppCompatActivity
+        implements SynchronyLights.HighLightButton,
+                   View.OnClickListener {
 
-    final int oneSec = 500;
+    private final int halfSec = 500;
+    private int score = 0;
+    private int index = 0;
+    private boolean gameStarted = false;
 
-    private Button red_btn;
-    private Button green_btn;
-    private Button yellow_btn;
-    private Button blue_btn;
+    private TransitionDrawable red_transition;
+    private TransitionDrawable green_transition;
+    private TransitionDrawable yellow_transition;
+    private TransitionDrawable blue_transition;
+
     private List<BtnColors> colorBtns = new LinkedList<>();
     private List<BtnColors> playerColorBtns = new LinkedList<>();
 
@@ -33,24 +40,39 @@ public class MainActivity extends AppCompatActivity implements SynchronyLights.H
         setContentView(R.layout.activity_main);
 
         final Context context = this;
-        red_btn = (Button) findViewById(R.id.btn_red);
-        green_btn = (Button) findViewById(R.id.btn_green);
-        yellow_btn = (Button) findViewById(R.id.btn_yellow);
-        blue_btn = (Button) findViewById(R.id.btn_blue);
+        Button red_btn = (Button) findViewById(R.id.btn_red);
+        Button green_btn = (Button) findViewById(R.id.btn_green);
+        Button yellow_btn = (Button) findViewById(R.id.btn_yellow);
+        Button blue_btn = (Button) findViewById(R.id.btn_blue);
+
+        red_btn.setOnClickListener(this);
+        green_btn.setOnClickListener(this);
+        yellow_btn.setOnClickListener(this);
+        blue_btn.setOnClickListener(this);
+
+        red_transition = (TransitionDrawable) red_btn.getBackground();
+        green_transition = (TransitionDrawable) green_btn.getBackground();
+        yellow_transition = (TransitionDrawable) yellow_btn.getBackground();
+        blue_transition = (TransitionDrawable) blue_btn.getBackground();
 
         final Button start_btn = (Button) findViewById(R.id.btn_start);
         start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                colorBtns.clear();
-                playerColorBtns.clear();
+                eraseGameData();
+                gameStarted = true;
                 getRandomButton();
-                for (BtnColors color: colorBtns) {
-                    new SynchronyLights(context, color).execute();
-                }
-
+                new SynchronyLights(context, colorBtns.get(0)).execute();
             }
         });
+    }
+
+    private void eraseGameData() {
+        score = 0;
+        index = 0;
+        colorBtns.clear();
+        playerColorBtns.clear();
+        gameStarted = false;
     }
 
     private void getRandomButton() {
@@ -58,47 +80,82 @@ public class MainActivity extends AppCompatActivity implements SynchronyLights.H
         int randomNumber = random.nextInt(4);
         switch (randomNumber) {
             case 0:
-//                Toast.makeText(this, "Red", Toast.LENGTH_SHORT).show();
                 colorBtns.add(RED);
                 break;
             case 1:
-//                Toast.makeText(this, "Green", Toast.LENGTH_SHORT).show();
                 colorBtns.add(GREEN);
                 break;
             case 2:
-//                Toast.makeText(this, "Yellow", Toast.LENGTH_SHORT).show();
                 colorBtns.add(YELLOW);
                 break;
             case 3:
-//                Toast.makeText(this, "Blue", Toast.LENGTH_SHORT).show();
                 colorBtns.add(BLUE);
                 break;
         }
     }
 
+    private void animateColoredButton(TransitionDrawable transition) {
+        transition.startTransition(halfSec);
+        transition.reverseTransition(halfSec);
+    }
+
     @Override
     public void onHighlightButtons(BtnColors color) {
-        TransitionDrawable red_transition = (TransitionDrawable) red_btn.getBackground();
-        TransitionDrawable green_transition = (TransitionDrawable) green_btn.getBackground();
-        TransitionDrawable yellow_transition = (TransitionDrawable) yellow_btn.getBackground();
-        TransitionDrawable blue_transition = (TransitionDrawable) blue_btn.getBackground();
         switch (color) {
             case RED:
-                red_transition.startTransition(oneSec);
-                red_transition.reverseTransition(oneSec);
+                animateColoredButton(red_transition);
                 break;
             case GREEN:
-                green_transition.startTransition(oneSec);
-                green_transition.reverseTransition(oneSec);
+                animateColoredButton(green_transition);
                 break;
             case YELLOW:
-                yellow_transition.startTransition(oneSec);
-                yellow_transition.reverseTransition(oneSec);
+                animateColoredButton(yellow_transition);
                 break;
             case BLUE:
-                blue_transition.startTransition(oneSec);
-                blue_transition.reverseTransition(oneSec);
+                animateColoredButton(blue_transition);
                 break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (gameStarted) {
+            switch (v.getId()) {
+                case R.id.btn_red:
+                    animateColoredButton(red_transition);
+                    playerColorBtns.add(RED);
+                    break;
+                case R.id.btn_green:
+                    animateColoredButton(green_transition);
+                    playerColorBtns.add(GREEN);
+                    break;
+                case R.id.btn_yellow:
+                    animateColoredButton(yellow_transition);
+                    playerColorBtns.add(YELLOW);
+                    break;
+                case R.id.btn_blue:
+                    animateColoredButton(blue_transition);
+                    playerColorBtns.add(BLUE);
+                    break;
+            }
+        }
+
+        if (!colorBtns.isEmpty() && !playerColorBtns.isEmpty())
+            if (colorBtns.get(index) != playerColorBtns.get(index)) {
+                Toast.makeText(this, "Perdiste, lograste " + score + " puntos", Toast.LENGTH_SHORT).show();
+                eraseGameData();
+            } else {
+                index++;
+            }
+
+        if (gameStarted && colorBtns.size() == playerColorBtns.size()) {
+            score++;
+            index = 0;
+            playerColorBtns.clear();
+            getRandomButton();
+            for (BtnColors color : colorBtns) {
+                new SynchronyLights(this, color).execute();
+            }
         }
     }
 }
